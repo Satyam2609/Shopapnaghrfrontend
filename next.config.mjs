fb5eb548fc02de02eb4061cdd1a4cb376ab6ae76
Satyam Jain <satyam26092004@gmail.com>
2025-11-08 19:28:32 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: { appDir: true },
+  srcDir: 'src', // tells Next.js that app folder is inside src
+};
 
-export default nextConfig;
+module.exports = nextConfig;
