ec62b4d1394b15f69d03c9dd8d3f84bc8dc3de0b
satyam jain <satyam260904@gmail.com>
2025-11-08 21:02:22 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: { appDir: true },
   srcDir: 'src',
+  images: {
+    domains: ['backend-production-6079.up.railway.app'], // backend domain yahan
+  },
 };
 
 export default nextConfig;
